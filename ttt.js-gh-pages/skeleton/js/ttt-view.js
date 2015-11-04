e304eb083ea7260451ea4ee638b9089f5ef37236
Jake Nelken <jnelks@gmail.com>
2015-11-04 15:09:51 -0500

    Complet JQuery TTT.

--- ttt.js-gh-pages/skeleton/js/ttt-view.js
+++ ttt.js-gh-pages/skeleton/js/ttt-view.js
@@ -10,38 +10,61 @@
   };
 
   View.prototype.bindEvents = function () {
-    this.$el.on("click", "li", function (e){
 
-      var $target = $(e.target);
+    // this.$el.on("click", "li", function (e) {
+    //   this.makeMove(e)
+    // }.bind(this));
+
+    this.$el.on("click", "li", this.makeMove.bind(this));
+
+    // this.$el.on("click", "li", function (e) {
+    //   var $target = $(e.target);
+    //   if (!$target.hasClass("x-square") && !$target.hasClass("o-square")) {
+    //     mark = this.game.currentPlayer;
+    //     $target.addClass(mark + "-square");
+    //     this.game.playMove($target.data("pos"));
+    //   } else {
+    //     alert("Stoopid move dood.");
+    //   };
+    //   if (this.game.winner){
+    //     alert(this.game.winner + "WINS!");
+    //   }
+    // }.bind(this));
+  };
+
+  // var $.prototype.on = function (eventName, cb) {
+  //   when eventName happens {
+  //     var theEvent = new $.Event();
+  //     cb(theEvent);
+  //   }
+  //
+  // }
+
+  View.prototype.makeMove = function ($square) {
+      var $target = $($square.target);
       if (!$target.hasClass("x-square") && !$target.hasClass("o-square")) {
-        mark = this.game.currentPlayer;
-        console.log(mark);
+        var mark = this.game.currentPlayer;
         $target.addClass(mark + "-square");
         this.game.playMove($target.data("pos"));
       } else {
         alert("Stoopid move dood.");
+      };
+      if (this.game.winner()) {
+        alert(this.game.winner().toUpperCase() + " WINS!");
+        location.reload();
       }
-
-
-    }.bind(this));
-  };
-
-  View.prototype.makeMove = function ($square) {
-
   };
 
   View.prototype.setupBoard = function () {
 
-    var $ul = $("<ul>").addClass("view group");
+    var $ul = $("<ul>").addClass("view group").appendTo(this.$el);
 
-    for (var i = 0; i < 3; i++){
-      for (var j = 0; j < 3; j++){
-        var cell = $("<li>");
-        cell.data("pos", [i, j]);
-        $ul.append(cell);
+    for (var i = 0; i < 3; i++) {
+      for (var j = 0; j < 3; j++) {
+        $("<li>")
+          .data("pos", [i, j])
+          .appendTo($ul);
       }
     }
-
-    this.$el.append($ul);
   };
 })();
